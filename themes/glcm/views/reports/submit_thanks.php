e7b3c909d3ee2ac611fd991a2991369a4d387ee0
jpistone <jpistonedev@gmail.com>
2014-03-05 22:11:56 -0500

    Oops... THIS closes #4

---
+++ themes/glcm/views/reports/submit_thanks.php
@@ -0,0 +1,13 @@
+<div id="content">
+	<div class="content-bg">
+		<!-- start block -->
+		<div class="big-block">
+			<h1><?php echo Kohana::lang('ui_main.reports_submit_new');?></h1>
+			<!-- green-box -->
+			<div class="green-box">
+				<h3><?php echo Kohana::lang('ui_main.reports_submitted');?></h3>
+			</div>
+		</div>
+		<!-- end block -->
+	</div>
+</div>
